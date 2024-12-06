547626f721b68499e06f1fc86c97307b285187a2
fort029 <117970032+fort029@users.noreply.github.com>
2024-12-06 11:58:46 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import { createServer } from 'node:http';
+import scrapeLogic  from "./appScrapeLogic.js";
+import url from 'url';
+const port = 80;
+const server = createServer((req, res) => {
+  let nr = url.parse(req.url, true).query['renderId'];
+  nr = Number(nr);
+  console.log(nr);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if(nr>0){
+    scrapeLogic(res, 1);
+  }else{
+    res.end(`Wrong id`);
+  }
+});
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
